4b9f8d420abeba1270dff85ac1ef2ef5b8150f0b
jgomezz <jgomezz@gmail.com>
2024-11-03 12:00:51 -0500

    Update

---
+++ src/main/java/pe/edu/tecsup/tienda/servlets/CategoriaListarServlet.java
@@ -0,0 +1,70 @@
+package pe.edu.tecsup.tienda.servlets;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import pe.edu.tecsup.tienda.entities.Categoria;
+import pe.edu.tecsup.tienda.services.CategoriaService;
+
+/**
+ * Servlet implementation class CategoriaListarServlet
+ */
+@WebServlet("/CategoriaListarServlet")
+public class CategoriaListarServlet extends HttpServlet {
+	
+	
+	private static final long serialVersionUID = 1L;
+
+	private static final Logger log = Logger.getLogger(CategoriaListarServlet.class);
+
+	private CategoriaService categoriaService;
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CategoriaListarServlet() {
+        super();
+        categoriaService = new CategoriaService();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		log.info("Call CategoriaListarServlet ");
+		
+		try {
+			
+			List<Categoria> categorias = categoriaService.listar();
+			
+			categorias.stream().forEach(x -> log.info(x));
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
