eb9eebd950c6b81fb81c76a6fb5c984741032009
jgomezz <jgomezz@gmail.com>
2024-11-03 12:51:26 -0500

    Update

---
+++ src/main/java/pe/edu/tecsup/tienda/servlets/ProductoListarServlet.java
@@ -0,0 +1,63 @@
+package pe.edu.tecsup.tienda.servlets;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import pe.edu.tecsup.tienda.entities.Producto;
+import pe.edu.tecsup.tienda.services.ProductoService;
+
+/**
+ * Servlet implementation class ProductoListarServlet
+ */
+@WebServlet("/ProductoListarServlet")
+public class ProductoListarServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final Logger log = Logger.getLogger(ProductoListarServlet.class);
+
+	private ProductoService productoService;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ProductoListarServlet() {
+		super();
+		this.productoService = new ProductoService();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		log.info("Call doGet()");
+		try {
+			List<Producto> productos = productoService.listar();
+			request.setAttribute("productos", productos);
+			request.getRequestDispatcher("/WEB-INF/jsp/producto/listar.jsp").forward(request, response);
+		} catch (Exception e) {
+			log.error(e, e);
+			throw new ServletException(e.getMessage(), e);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
